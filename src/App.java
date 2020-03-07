dbfddc8ea8725a382059e787745eb37a8042ec7c
Reset <Reset@NB-004230.cwinet.local>
2020-03-07 10:56:22 -0300

    criação do projeto

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+     public static void main (String[] args) {
+System.out.println("Hello World");
+          System.out.println("outras coisas");
+          System.out.println("novas linhas");
+          System.out.println("mensagens");
+     }
+}
